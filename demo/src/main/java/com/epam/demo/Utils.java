28fe3a26fa3f44a9561c1261c38eb5e93dbd7ce8
kislyakovm <kislyakovm97@gmail.com>
2024-06-13 01:24:26 +0500

    update

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        if (args.contains(null) || args == null) return false;
+
+        for (String str : args) {
+            if (str.startsWith("-") || str.equals("0") || str.startsWith(" ") || str.equals("")) return false;
+        }
+        return true;
     }
 }
